21cf41194fb7b1040fc21d39bcc6e532e4340d40
Max <kittiphot.de@gmail.com>
2021-12-23 00:15:17 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+
+const app = express();
+// express server cant use static files if not stated
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",(req,res)=>{
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  }
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us20.api.mailchimp.com/3.0/lists/"+process.env.LIST_ID;
+  const options = {
+    method: "POST",
+    auth: "key:"+process.env.API_KEY
+  }
+
+  const request = https.request(url,options, (response)=>{
+    if(response.statusCode === 200){
+      res.send("Successfully subscribed.");
+    }else{
+      res.send("There was an error, please try again!")
+    }
+    response.on("data", (data)=>{
+      console.log(JSON.parse(data));
+    })
+  });
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.listen(process.env.PORT||3000, ()=>{
+  console.log("Server is running on port 3000.");
+});
